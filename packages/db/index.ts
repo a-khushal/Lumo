38210e3f2f2150ec055e93416493d75938c95010
Khushal <khushal2004a@gmail.com>
2026-03-08 11:08:13 +0530

    feat: prisma singleton client

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client";
+
+type GlobalForDb = {
+  db?: PrismaClient;
+};
+
+const globalForDb = globalThis as GlobalForDb;
+
+const createDbClient = () => {
+  const databaseUrl = process.env.DATABASE_URL;
+
+  if (!databaseUrl) {
+    throw new Error("DATABASE_URL is not set");
+  }
+
+  return new PrismaClient({
+    adapter: new PrismaPg({ connectionString: databaseUrl }),
+  });
+};
+
+export const db = globalForDb.db ?? createDbClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForDb.db = db;
+}
+
+export { Prisma } from "./generated/prisma/client";
